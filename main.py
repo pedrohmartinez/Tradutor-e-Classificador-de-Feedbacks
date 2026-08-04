55945fe640e9ef66a4a4a574a98046c1543ed445
Pedro Henrique Martinez <henriquepedromartinez@gmail.com>
2026-08-04 14:43:53 -0300

    descrição do desafio

---
+++ main.py
@@ -0,0 +1,8 @@
+# Desafio Final (usando uma IDE):
+# 1-Carregar um arquivo .txt, onde cada linha será um elemento de uma lista do Python
+# 2-Mandá-la ao modelo que você está rodando localmente para extrair, em formato JSON, onde cada item terá "usuario", "resenha original", "resenha_pt", "avaliacao" (Positiva, Negativa, Neutra)
+# 3-Transformar a resposta do modelo em uma lista de dicionários Python
+# 4-Criar uma função que, dada uma lista de dicionários, percorre a lista faz 2 coisas:
+# a) conta a quantidade de avaliações positivas, negativas e neutras,
+# b) une cada item dessa lista em uma variável do tipo string com algum separador.
+# Ao final, retorna ambas as coisas.
